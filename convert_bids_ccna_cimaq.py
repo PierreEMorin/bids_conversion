17338a42dfaa36b1c1a7dd2bcf570e9623d349ac
Pierre-Emmanuel Morin <pierre-emmanuel.morin@criugm.qc.ca>
2020-12-22 16:11:41 -0500

    Ajout de 'mc_connell_brain_imaging_centre' dans CIMAQ_SIEMENS_CONFIG

--- convert_bids_ccna_cimaq.py
+++ convert_bids_ccna_cimaq.py
@@ -21,6 +21,7 @@ PHILIPS_CONFIG = os.path.join(CONFIG_FOLDER, 'config_philips_cimaq.json')
 QUEBEC_CONFIG = os.path.join(CONFIG_FOLDER, 'config_philips_cimaq_QC.json')
 CIMAQ_SIEMENS_CONFIG = ['IUGM', 'iugm', 'hospital_douglas',
                         'Mc_Connell_Brain_Imaging_Centre',
+                        'mc_connell_brain_imaging_centre',
                         'THE_OTTAWA_HOSPITAL_CIVIC',
                         'the_ottawa_hospital_civic_campus']
 TIME_SLEEP = 1
